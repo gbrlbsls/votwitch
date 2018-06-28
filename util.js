fa8d37cef28c21840781671a24830f1551e1de20
Gabriel Sales <gabrielbsales@gmail.com>
2018-06-27 21:04:43 -0300

    Update util.js

--- util.js
+++ util.js
@@ -1,11 +1,13 @@
-function findHashParameter(parameterName) {
+function findGetParameter(parameterName) {
     var result = null,
         tmp = [];
-    var items = location.hash.substr(1).split("&");
-    for (var index = 0; index < items.length; index++) {
-        tmp = items[index].split("=");
-        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-    }
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
     return result;
 }
 
